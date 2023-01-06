76acfb088622eea8239cd8d30da77355750e7352
s16121986 <s16121986@yandex.ru>
2023-01-06 14:49:46 +0300

    init

---
+++ Tag/Sitemap.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Gsdk\Sitemap\Tag;
+
+use Gsdk\Sitemap\AbstractParent;
+
+class Sitemap extends AbstractParent {
+
+	protected $tag = 'sitemap';
+
+	protected function init() {
+		$this
+			->addTag('loc')
+			->addTag('lastmod');
+	}
+
+
+}
